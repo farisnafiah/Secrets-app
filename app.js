35fe7647a224ea107bd5dfdc778bc9a3256a6ec5
Faris Nafiah <nafiahf@lsbu.ac.uk>
2020-05-31 21:13:03 +0800

    Level 4 - Salting and Hashing. md5 hash has been disregarded.

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-mongoose.connect("mongodb://localhost:27017/userDB",
-{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
@@ -35,17 +40,23 @@ app.route("/login")
   })
   .post(function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password)
+    const password = req.body.password
 
-    User.findOne({email: username}, function(err, foundUser) {
+    User.findOne({
+      email: username
+    }, function(err, foundUser) {
       if (err) {
         console.log(err);
       } else {
         if (foundUser) {
-          if (foundUser.password === password) {
-            console.log(password);
-            res.render("secrets");
-          }
+          bcrypt.compare(password, foundUser.password, function (err, result) {
+            if (result ===  true) {
+              res.render("secrets");
+            } else {
+              console.log("Wrong password");
+              res.render("login");
+            }
+          });
         }
       }
     })
@@ -58,18 +69,22 @@ app.route("/register")
     res.render("register");
   })
   .post(function(req, res) {
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password)
-    });
 
-    newUser.save(function(err){
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      // Store hash in your password DB.
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
+
+      newUser.save(function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets");
+        }
+      });
+    });
 
   });
 ///////
