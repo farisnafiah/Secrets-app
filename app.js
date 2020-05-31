49cad005312c54d507ecc33ea2762d99d44ad249
Faris Nafiah <nafiahf@lsbu.ac.uk>
2020-05-31 23:52:50 +0800

    Finishing up this Whisper-like app for users to submit their secrets. Pheww

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -122,11 +123,44 @@ app.route("/register")
 
 app.route("/secrets")
   .get(function(req, res) {
+    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUsers) {
+          res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+      }
+    });
+  });
+////
+
+app.route("/submit")
+  .get(function(req, res) {
     if (req.isAuthenticated()) {
-      res.render("secrets");
+      res.render("submit");
     } else {
       res.redirect("/login");
     }
+  })
+  .post(function(req, res) {
+    const submittedSecret = req.body.secret;
+
+    console.log(req.user.id);
+
+    User.findById(req.user.id, function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          foundUser.secret = submittedSecret;
+          foundUser.save(function() {
+            res.redirect("/secrets");
+          });
+        }
+      }
+    });
+
   });
 ////
 
